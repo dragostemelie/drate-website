5a742c65340525feff061138f6bcdac34c9603ed
Dragos Temelie <dragostemelie@gmail.com>
2023-11-12 12:38:36 +0200

    Update

--- deply-to-production.js
+++ deply-to-production.js
@@ -15,10 +15,11 @@ const htmlText = fs.readFileSync(HTML_FILE, 'utf8');
 const scriptTag = htmlText.match(scriptRegex)?.[0];
 const linkTag = htmlText.match(linkRegex)?.[0];
 
-if (scriptTag !== undefined && linkTag !== undefined) {
+if (scriptTag !== undefined && linkTag !== undefined && false) {
     const phpText = fs.readFileSync(PHP_FILE, 'utf8');
     const newPhpText = phpText.replace(scriptRegex, scriptTag).replace(linkRegex, linkTag);
-    // console.log("newPhpText", newPhpText);
+    console.log("Updating index.php")
+    fs.writeFileSync(PHP_FILE, newPhpText);
 }
 
 // Read uncommited files
@@ -27,28 +28,32 @@ const data = await git.status('s')
 
 // Upload last commited files to production server
 const FILES = data.files.filter(file => file.working_dir !== "D").map(file => file.path)
+console.log("Files to be uploaded:", FILES)
 
-// if (false == undefined) {
-//     const FILES = [
-//         "test.txt", //
-//     ]
-//     const client = new ftp.Client()
-//     client.ftp.verbose = true;
-//     try {
-//         await client.access({
-//             host: process.env.FTP_HOST,
-//             user: process.env.FTP_USER,
-//             password: process.env.FTP_PASS,
-//             secure: true
-//         })
-//         console.log(await client.list())
+if (FILES.length > 0) {
+    const client = new ftp.Client()
+    client.ftp.verbose = false;
+    try {
+        await client.access({
+            host: process.env.FTP_HOST,
+            user: process.env.FTP_USER,
+            password: process.env.FTP_PASS,
+            secure: true
+        })
+        // console.log(await client.list())
 
-//         for (const file of FILES) {
-//             await client.uploadFrom(file, "public_html/" + file)
-//         }
-//     }
-//     catch (err) {
-//         console.log(err)
-//     }
-//     client.close()
-// }
+        for (const file of FILES) {
+            const folderPath = file.match(/(.*)\//)?.[1];
+            if (folderPath !== undefined) {
+                await client.ensureDir("public_html/" + folderPath)
+            }
+            await client.cd("/")
+            console.log("Uploading public_html/" + file)
+            await client.uploadFrom(file, "public_html/" + file)
+        }
+    }
+    catch (err) {
+        console.log(err)
+    }
+    client.close()
+}
